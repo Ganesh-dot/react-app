20f58f7bf00b785536f243fffd57d1d164495349
@Ganesh-dot <gsk1664@gmail.com>
2020-05-02 17:00:46 +0530

    my first attempting on react

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        
-        <p>
-          First react app from ganesh
+      <header className="App-header">  
+        <p className="header-text">
+          React app
         </p>
+      
       </header>
+      <body>
+              
+      </body>
     </div>
   );
 }
